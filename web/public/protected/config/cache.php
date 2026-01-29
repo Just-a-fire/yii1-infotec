a6ed0575f7516b291dbfc356a28fb64556881698
Albert Developer <albert.developer@yandex.ru>
2026-01-29 04:10:21 +0300

    Установил пароль на редис

--- web/public/protected/config/cache.php
+++ web/public/protected/config/cache.php
@@ -3,9 +3,9 @@
 return [
 	'class' => 'CRedisCache',
     'hostname' => getenv('REDIS_HOSTNAME'),
-    'port' => 6379,
+    'port' => getenv('REDIS_PORT'),
     'database' => 0,
     // 'options' => STREAM_CLIENT_CONNECT,
     // 'username' => 'default',
-    // 'password' => getenv('REDIS_PASSWORD')
+    'password' => getenv('REDIS_PASSWORD')
 ];
